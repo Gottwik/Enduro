7dc9c8c2a637753232c473ed85dc5dce9d9687af
gottwik <gottwik@gmail.com>
2016-02-03 11:50:00 +0100

    console

--- libs/admin_api/save_cms.js
+++ libs/admin_api/save_cms.js
@@ -7,6 +7,7 @@ var api_call = function () {}
 api_call.prototype.call = function(req, res, query){
 	var file = query.cms_name
 	var contents = 'module.exports = ' + query.contents
+	console.log(contents)
 	fs.writeFile( process.cwd() + '/cms/' + file + '.js' , contents, function(err) {
 		if (err) { return req.send(err) }
 		res.send('success')

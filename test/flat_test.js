979f169676fd12757b0f6738cb579fbf1eb8a5f3
gottwik <gottwik@gmail.com>
2016-02-29 23:59:44 +0100

    added flat tests

--- test/flat_test.js
+++ test/flat_test.js
@@ -1,7 +1,7 @@
 var expect = require("chai").expect
 var fs = require('fs')
 var enduro_helpers = require('../libs/flat_utilities/enduro_helpers')
-
+var rimraf = require('rimraf')
 
 describe('Enduro flat file utilities', function() {
 
@@ -19,6 +19,33 @@ describe('Enduro flat file utilities', function() {
 
 	it('should not detect an nonexisting file in subfolder', function () {
 		expect(enduro_helpers.fileExists(cmd_folder + '/libs/crazyfile.js')).to.not.equal(true)
+	})
+
+	it('should detect an existing folder', function () {
+		expect(enduro_helpers.dirExists(cmd_folder + '/libs')).to.equal(true)
+	})
+
+	it('should detect an existing nested folder', function () {
+		expect(enduro_helpers.dirExists(cmd_folder + '/scaffolding/api')).to.equal(true)
+	})
+
+	it('should not detect an nonexisting folder', function () {
+		expect(enduro_helpers.dirExists(cmd_folder + '/asdf')).to.not.equal(true)
+	})
+
+	it('should create all neccessary subdirectories', function () {
+		enduro_helpers.ensureDirectoryExistence(cmd_folder + '/test_folder/subfolder/test.js')
+			.then(function(){
+				expect(enduro_helpers.dirExists(cmd_folder + '/test_folder/subfolder')).to.equal(true)
+			}, function(err){
+				expect(true).to.equal(false)
+			})
+	})
+
+	it('should delete all test folders', function () {
+		rimraf(cmd_folder + '/test_folder', function(err){
+			expect(enduro_helpers.dirExists(cmd_folder + '/test_folder')).to.not.equal(true)
+		})
 	})
 
 })

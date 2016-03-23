452c9b3d31be45959764244f86f56b0e04c73503
gottwik <gottwik@gmail.com>
2016-03-23 18:36:12 +0100

    fixed partial helper

--- hbs_helpers/partial.js
+++ hbs_helpers/partial.js
@@ -1,14 +1,17 @@
 __templating_engine.registerHelper("partial", function (name, options) {
+
 	// Get the partial with the given name. This is a string.
 	var partial = __templating_engine.partials[name]
 	// Return empty string if the partial is not defined
 	if (!partial) return ""
 
 	// build up context
-	context = {}
-	context.context = this
+	context = this
 	context.global = options.data.root
 
 	// Compile and call the partial with context
-	return new __templating_engine.SafeString(__templating_engine.compile(partial)(context))
+	return (typeof partial == 'function')
+		? new __templating_engine.SafeString(partial(context))
+		: new __templating_engine.SafeString(__templating_engine.compile(partial)(context))
+
 });

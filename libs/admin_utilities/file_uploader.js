967e4f4137d4bc2773fc06aebf7ed82ede3c2bdf
Martin Gottweis <gottwik@gmail.com>
2016-05-24 11:35:40 +0200

    server build debug

--- libs/admin_utilities/file_uploader.js
+++ libs/admin_utilities/file_uploader.js
@@ -18,10 +18,10 @@ admin_file_upload_handler.prototype.upload = function(file) {
 	return new Promise(function(resolve, reject){
 
 		var destination_path = CMD_FOLDER + UPLOADS_FOLDER + '/' + file.name
-		var destination_src_path = CMD_FOLDER + UPLOADS_FOLDER + '/_src/' + file.name
+		var destination_src_path = CMD_FOLDER + '/_src/' +UPLOADS_FOLDER + file.name
 		var destination_url = UPLOADS_FOLDER + '/' + file.name
 
-		enduro_helpers.ensureDirectoryExistence(destination_path)
+		enduro_helpers.ensureDirectoryExistence(destination_src_path)
 			.then(() => {
 				//fs.rename(file.path, destination_path, function(err) {
 				console.log('copyying file', file.path, destination_path)

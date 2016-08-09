dea6e01d1d40eefc2f934e8109b7e1603fe86549
Martin Gottweis <gottwik@gmail.com>
2016-08-09 14:13:04 +0200

    removed obsolete code from grouped_each helper

--- hbs_helpers/grouped_each.js
+++ hbs_helpers/grouped_each.js
@@ -14,27 +14,15 @@ __templating_engine.registerHelper('grouped_each', function(every, context, opti
 	var subcontext = []
 	var i = 0
 
-	if(typeof context === 'object') { // Context is an object
-		for (var key in context) {
-			if (i > 0 && i % every === 0) {
-				out += options.fn(subcontext)
-				subcontext = []
-			}
-			subcontext.push(context[key])
-			i++
+	for (var key in context) {
+		if (i > 0 && i % every === 0) {
+			out += options.fn(subcontext)
+			subcontext = []
 		}
-		out += options.fn(subcontext)
-	} else { // Context is array
-		for (i = 0; i < context.length; i++) {
-			if (i > 0 && i % every === 0) {
-				out += options.fn(subcontext)
-				subcontext = []
-			}
-			subcontext.push(context[i])
-		}
-		out += options.fn(subcontext)
+		subcontext.push(context[key])
+		i++
 	}
-
+	out += options.fn(subcontext)
 
 	// Outputs processed html
 	return out
---
+++ test/helpers/grouped_each_test.js
@@ -0,0 +1,34 @@
+// vendor dependencies
+var expect = require("chai").expect
+
+global.__templating_engine = require('handlebars')
+
+describe('Grouped_each helper', function() {
+
+	var test_input1 = '{{#grouped_each 3 list}}a{{#each this}}{{this}}{{/each}}{{/grouped_each}}'
+	var test_output1 = 'a123a456'
+	var test_context1 = {list: [1,2,3,4,5,6]}
+
+	it('render every third item', function () {
+		expect(__templating_engine.compile(test_input1)(test_context1)).to.equal(test_output1)
+	})
+
+
+	var test_input2 = '{{#grouped_each 3 this}}a{{#each this}}{{this}}{{/each}}{{/grouped_each}}'
+	var test_output2 = 'a123a456'
+	var test_context2 = [1,2,3,4,5,6]
+
+	it('render every third item when array is provided', function () {
+		expect(__templating_engine.compile(test_input2)(test_context2)).to.equal(test_output2)
+	})
+
+
+	var test_input3 = '{{#grouped_each 2 this}}a{{#each this}}{{this}}{{/each}}{{/grouped_each}}'
+	var test_output3 = 'a12a34a56'
+	var test_context3 = {a: 1, b: 2, c: 3, d: 4, e: 5, f: 6}
+
+	it('render every third item when object is provided', function () {
+		expect(__templating_engine.compile(test_input3)(test_context3)).to.equal(test_output3)
+	})
+
+})

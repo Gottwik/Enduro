af0123de4c55523724ca0aa7604d95e825a01dce
Martin Gottweis <gottwik@gmail.com>
2016-09-08 08:28:23 +0200

    regenerates cms list upon adding a new page

--- libs/admin_utilities/page_adding_service.js
+++ libs/admin_utilities/page_adding_service.js
@@ -4,22 +4,19 @@
 var page_adding_service = function () {}
 
 // vendor dependencies
-var Promise = require('bluebird')
 var path = require('path')
 
 // local dependencies
 var flat_file_handler = require(ENDURO_FOLDER + '/libs/flat_utilities/flat_file_handler')
 
 page_adding_service.prototype.new_generator_page = function (new_pagename, generator) {
-	return new Promise(function (resolve, reject) {
-		flat_file_handler.load(path.join('generators', generator, generator))
-			.then((template_content) => {
-				return flat_file_handler.save(path.join('generators', generator, new_pagename), template_content)
-			})
-			.then(() => {
-				resolve()
-			})
-	})
+	return flat_file_handler.load(path.join('generators', generator, generator))
+		.then((template_content) => {
+			return flat_file_handler.save(path.join('generators', generator, new_pagename), template_content)
+		})
+		.then(() => {
+			return generate_cms_list()
+		})
 }
 
 module.exports = new page_adding_service()

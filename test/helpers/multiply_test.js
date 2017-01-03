bc3bf83c2a296c757bcfd18661e2ef187b6448c0
Martin Gottweis <gottwik@gmail.com>
2017-01-03 15:54:26 +0100

    added multiply and slug helpers

---
+++ test/helpers/multiply_test.js
@@ -0,0 +1,18 @@
+// vendor dependencies
+var expect = require('chai').expect
+
+describe('Multiply helper', function () {
+
+	it('should multiply two numbers', function () {
+		expect(__templating_engine.compileSync('{{multiply 2 2}}')()).to.equal('4')
+	})
+
+	it('should multiply three numbers', function () {
+		expect(__templating_engine.compileSync('{{multiply 2 2 2}}')()).to.equal('8')
+	})
+
+	it('should multiply numbers from context', function () {
+		expect(__templating_engine.compileSync('{{multiply first second}}')({first: 3, second: 5})).to.equal('15')
+	})
+
+})

700875141810cdf7a0c624bb1040c401cb566030
Martin Gottweis <gottwik@gmail.com>
2017-03-13 19:19:02 +0100

    added timeout to tests

--- test/abstractor_test.js
+++ test/abstractor_test.js
@@ -7,6 +7,7 @@ var abstractor = require(enduro.enduro_path + '/libs/abstractor/abstractor')
 var test_utilities = require('./libs/test_utilities')
 
 describe('Abstractor', function () {
+	this.timeout(7000) // this is currently the first test of all and sometimes the initial enduro config read is slower than the default 2000ms
 
 	before(function () {
 		return test_utilities.before(local_enduro, 'abstractor_testfolder')

897fd54cf0600d74ca6ad15b806e30a3c791af20
Martin Gottweis <gottwik@gmail.com>
2017-06-08 09:28:52 +0200

    moved less autoprefixer to dependencies

--- cli.js
+++ cli.js
@@ -1,9 +1,10 @@
 #!/usr/bin/env node
 
+// this is the entry point for cli commands
+// each command is a corresponding file in the /cli_commands folder strucutre
 global.program = require('yargs')
-	.commandDir('cli_commands')
-	.version(require('./package.json').version)
-	.help()
-	.alias('help', 'h')
-	.wrap(120)
+	.commandDir('cli_commands') // sets /cli_commands as source for the commands
+	.version(require('./package.json').version) // fetches the current version from package.json
+	.help().alias('help', 'h') // creates help command with a -h alias
+	.wrap(120) // wraps lines at 120 characters
 	.argv
--- index.js
+++ index.js
@@ -4,33 +4,51 @@
 // * ———————————————————————————————————————————————————————— * //
 
 // vendor dependencies
-var Promise = require('bluebird')
+const Promise = require('bluebird')
 
-var linker = require('./libs/linker/linker')
+// local dependencies
+const linker = require('./libs/linker/linker')
 
-var enduro_instance = function () {}
+const enduro_instance = function () {}
 
+// * ———————————————————————————————————————————————————————— * //
+// * 	quick_init
+// *
+// * 	sets up limited global variables such as enduro's location
+// *	this is used mainly to speed up tests where all enduro's variables
+// *	are not needed
+// *	@return nothing
+// * ———————————————————————————————————————————————————————— * //
 enduro_instance.prototype.quick_init = function () {
 
 	// exposes enduro api, state, variables and configuration as public variable
-	// to cut down on complexity when developing enduro projects
 	global.enduro = linker.init_enduro_linked_configuration(process.cwd(), __dirname)
 
-	return this
+	return this // returns self chain-style in case the full init is needed later
 }
 
+// * ———————————————————————————————————————————————————————— * //
+// * 	init
+// *
+// * 	- sets up global enduro variables
+// *	- exposes enduro's api to client project
+// *	- exposes enduro's main action
+// *	- read and stores project-specific configuration
+// *	@return {Promise} - resolves after enduro is ready to start
+// * ———————————————————————————————————————————————————————— * //
 enduro_instance.prototype.init = function (project_path) {
 
-	var new_project_path = project_path || process.cwd()
+	const new_project_path = project_path || process.cwd()
 
 	// exposes enduro api, state, variables and configuration as public variable
-	// to cut down on complexity when developing enduro projects
 	global.enduro = linker.init_enduro_linked_configuration(new_project_path, __dirname)
 
 	// exposes enduro's api libraries and action functions
 	linker.expose_enduro_api()
+
+	// these two are asynchronous, so let's do them in parallel
 	return Promise.all([
-		linker.expose_enduro_actions(),
+		linker.expose_enduro_actions(), // actions are stored in /libs/actions/
 		linker.read_config()
 	])
 }

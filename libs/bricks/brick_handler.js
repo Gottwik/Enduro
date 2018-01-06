91e9c7869dd251d7c3fc047548f06cc6ff3b943e
Martin Gottweis <gottwik@gmail.com>
2018-01-07 00:51:03 +0100

    added brick admin settings passing into js

--- libs/bricks/brick_handler.js
+++ libs/bricks/brick_handler.js
@@ -5,7 +5,13 @@
 // * ———————————————————————————————————————————————————————— * //
 const brick_handler = function () {}
 
+// * vendor dependencies
 const path = require('path')
+
+// * enduro dependencies
+const flat = require(enduro.enduro_path + '/libs/flat_db/flat')
+const Promise = require('bluebird')
+const fs = Promise.promisifyAll(require('fs-extra'))
 
 // * ———————————————————————————————————————————————————————— * //
 // * 	loads, initialize and pre-store brick-related data
@@ -15,17 +21,28 @@ brick_handler.prototype.load_bricks = function () {
 	const all_bricks = enduro.config.bricks
 
 	enduro.config.brick_admin_injects = []
+	
+	// will gather all brick's settings which will be dumped into js file
+	let brick_admin_settings = {}
+
 	for (brick_name in all_bricks) {
 		const brick_instance = self._require_brick(brick_name)
+		const brick_settings = all_bricks[brick_name]
 		
 		if (brick_instance.init) {
-			brick_instance.init()
+			brick_instance.init(brick_settings)
 		}
 
 		if (brick_instance.brick_configuration.admin_js_inject) {
 			enduro.config.brick_admin_injects.push('/brick/' + brick_name + '/' + brick_instance.brick_configuration.admin_js_inject)
 		}
+
+		// add settings to global admin js settings endpoint
+		brick_admin_settings[brick_name] = brick_settings
 	}
+	const brick_admin_settings_path = path.join(enduro.project_path, enduro.config.build_folder, '/_prebuilt/brick_admin_settings.js');
+	const brick_admin_settings_string_to_save = 'var brick_admin_settings = ' + JSON.stringify(brick_admin_settings)
+	return fs.writeFileAsync(brick_admin_settings_path, brick_admin_settings_string_to_save);
 }
 
 // * ———————————————————————————————————————————————————————— * //
